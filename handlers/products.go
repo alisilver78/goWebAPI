691a24536feef2cb566ddef828d04e07071b76c4
Ali <alireza.mg.g@gmail.com>
2022-09-04 15:24:10 +0430

    adding CreateProducts func for its method

--- handlers/products.go
+++ handlers/products.go
@@ -1,2 +1,52 @@
 package handlers
 
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/alisilver78/goWebAPI/dbiface"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// product define an electronic product
+type Product struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"product_name" bson:"product_name"`
+	Price       int                `json:"price" bson:"price"`
+	Currency    string             `json:"currency" bson:"currency"`
+	Quantity    int                `json:"quantity" bson:"quantity"`
+	Discount    int                `json:"discount,omitempty" bson:"discount,omitempty"`
+	Vendor      string             `json:"vendor" bson:"vendor"`
+	Accessories []string           `json:"accessories,omitempty" bson:"accessories,omitempty"`
+}
+
+// ProductHandler is a product handler
+type ProductHandler struct {
+	Col dbiface.CollectionAPI
+}
+
+func CreateProducts(c echo.Context, products []Product, collection dbiface.CollectionAPI) ([]interface{}, error) {
+	var insertedIds []interface{}
+	for _, product := range products {
+		product.ID = primitive.NewObjectID()
+		InsertID, err := collection.InsertOne(context.Background(), product)
+		if err != nil {
+			log.Printf("Unable to insert: %v", err)
+			return nil, err
+		}
+		insertedIds = append(insertedIds, InsertID.InsertedID)
+	}
+	return insertedIds, nil
+}
+
+// CreateProducts creates products
+func (h *ProductHandler) CreateProducts(c echo.Context) error {
+	var products []Product
+	if err := c.Bind(&products); err != nil {
+		log.Printf("Unable to bind: %v", err)
+	}
+
+	return c.JSON(http.StatusCreated, "created")
+}
--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/alisilver78/goWebAPI/config"
+	"github.com/alisilver78/goWebAPI/handlers"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	c   *mongo.Client
+	db  *mongo.Database
+	col *mongo.Collection
+	cfg config.Properties
+)
+
+func init() {
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		log.Fatalf("Configuration cannot be read: %v", err)
+	}
+	connectURI := fmt.Sprintf("mongodb://%s:%s", cfg.DBHost, cfg.DPPort)
+	c, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
+	if err != nil {
+		log.Fatalf("Unable connect to database: %v", err)
+	}
+
+	db = c.Database(cfg.DBName)
+	col = db.Collection(cfg.CollectionName)
+}
 
 func main() {
 	e := echo.New()
-	e.POST("/products", createProduct)
-	
+	h := handlers.ProductHandler{Col: col}
+	e.POST("/products", h.CreateProducts)
+
+	e.Logger.Infof("listening on: %s:%s", cfg.Host, cfg.Port)
+	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)))
+
 }
