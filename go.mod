46d04fd51cb32dcb350888112c2df9b6fd0d6d25
Ali <alireza.mg.g@gmail.com>
2022-09-04 22:52:02 +0430

    Post method's Integrating test  created

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.3.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
@@ -34,6 +35,7 @@ require (
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
---
+++ handlers/products_test.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/alisilver78/goWebAPI/config"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	c   *mongo.Client
+	db  *mongo.Database
+	col *mongo.Collection
+	cfg config.Properties
+	h   ProductHandler
+)
+
+func init() {
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		log.Fatalf("Configuration cannot be read: %v", err)
+	}
+	connectURI := fmt.Sprintf("mongodb://%s:%s", cfg.DBHost, cfg.DPPort)
+	var err error
+	c, err = mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
+	if err != nil {
+		log.Fatalf("Unable connect to database: %v", err)
+	}
+
+	db = c.Database(cfg.DBName)
+	col = db.Collection(cfg.CollectionName)
+}
+
+// integration test
+func TestProduct(t *testing.T) {
+	t.Run("testing product", func(t *testing.T) {
+		body := `
+		[{
+			"product_name":"Google Nest Thermostat",
+			"price":98,
+			"currency":"USD",
+			"vendor":"Google",
+			"accessories":["wire", "subscription" ]
+		}]`
+		req := httptest.NewRequest("POST", "/producs", strings.NewReader(body))
+		res := httptest.NewRecorder()
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		e := echo.New()
+		c := e.NewContext(req, res)
+		h.Col = col
+		err := h.CreateProducts(c)
+		assert.Nil(t, err)
+	})
+
+}
