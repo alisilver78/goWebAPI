ef8655597352b2c7964d068e82acd2d8504238cf
Ali <alireza.mg.g@gmail.com>
2022-09-06 00:21:45 +0430

    adding Correlation ID as X-Correlation-ID

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/random"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	CorrelationID = "X-Correlation-ID"
 )
 
 var (
@@ -26,7 +31,8 @@ func init() {
 		log.Fatalf("Configuration cannot be read: %v", err)
 	}
 	connectURI := fmt.Sprintf("mongodb://%s:%s", cfg.DBHost, cfg.DPPort)
-	c, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
+	var err error
+	c, err = mongo.Connect(context.Background(), options.Client().ApplyURI(connectURI))
 	if err != nil {
 		log.Fatalf("Unable connect to database: %v", err)
 	}
@@ -35,11 +41,31 @@ func init() {
 	col = db.Collection(cfg.CollectionName)
 }
 
+func addCorrelationID(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		//generate correlation id
+		var newID string
+		id := c.Request().Header.Get(CorrelationID)
+		if id == "" {
+			//generate id
+			newID = random.String(12)
+		} else {
+			//assigen id
+			newID = id
+		}
+		c.Request().Header.Set(CorrelationID, newID)
+		c.Response().Header().Set(CorrelationID, newID)
+
+		return next(c)
+	}
+}
+
 func main() {
 	e := echo.New()
 	h := handlers.ProductHandler{Col: col}
 
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Pre(addCorrelationID)
 	e.POST("/products", h.CreateProducts, middleware.BodyLimit("1M"))
 
 	e.Logger.Infof("listening on: %s:%s", cfg.Host, cfg.Port)
