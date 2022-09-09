0ee8495b008a997cfafe216b653c4449ca684111
Ali <alireza.mg.g@gmail.com>
2022-09-09 18:15:30 +0430

    add hashing password methods

--- handlers/users.go
+++ handlers/users.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/gommon/log"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/go-playground/validator.v9"
 )
 
@@ -44,6 +45,12 @@ func insertUser(ctx context.Context, user User, collection dbiface.CollectionAPI
 		log.Errorf("User by %v already exists", newuser.Email)
 		return nil, echo.NewHTTPError(http.StatusBadRequest, "User already exists")
 	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 8)
+	if err != nil {
+		log.Errorf("Unable to process the error: %v", err)
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "Unable to process the error")
+	}
+	user.Password = string(hashedPassword)
 	insertRes, err := collection.InsertOne(ctx, user)
 	if err != nil {
 		log.Errorf("Unable to create user: %v", err)
