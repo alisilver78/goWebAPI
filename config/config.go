318c6c0ec50bf13de3ef64826ffee7f33f2382c6
Ali <alireza.mg.g@gmail.com>
2022-09-17 14:05:52 +0430

    default port changed to 8080

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 // configuration properties based on env variables
 type Properties struct {
-	Port               string `env:"MY_APP_PORT" env-default:"8081"`
+	Port               string `env:"MY_APP_PORT" env-default:"8080"`
 	Host               string `env:"HOST" env-default:"localhost"`
 	DBHost             string `env:"DB_HOST" env-default:"localhost"`
 	DPPort             string `env:"DB_PORT" env-default:"27017"`
